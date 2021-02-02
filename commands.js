c154f099293ec6226f0248b1639182c9d367916c
michael tereschenko <michi.tereschenko@gmail.com>
2021-02-02 22:04:37 +0100

    added ranking and leveling and distributing roles

--- commands.js
+++ commands.js
@@ -1,26 +1,32 @@
-module.exports = async function(msg, arguments) {
+module.exports = function(msg, arguments) {
 
     function getXpToLevelUp(xp) {
-        console.log("xp");
-        return "**sehr viel** xp bist zum level **Master Guardian**"
+        return `**${1000-(xp%1000)}** xp bis zum level **${Math.ceil(xp/1000)}**`;
     }
     let args = msg.content.split(" ");
     let command = args.shift();
     if (msg.author.bot && command.startsWith("ẞ")) return;
     command = command.substring(1);
-    console.log(command, args);
     if (msg.channel.id == "712243995689877544" && command == "ping") {
         msg.reply("pong");
     }
     if (command == "rank") {
-        let mentioned = args[0].substring(3, args[0].length-1);
-        console.log(mentioned);
-        arguments.db.findOne({ userid: mentioned || msg.author.id }, function (err, doc) {
+        let mentionedname;
+        let mentionedid;
+        let men = msg.mentions.users.first();
+        if (men) {
+            mentionedname = men.username;
+            mentionedid = men.id;
+        } else {
+            mentionedname = msg.author.username;
+            mentionedid = msg.author.id;
+        }
+        arguments.db.findOne({ userid: mentionedid}, function (err, doc) {
             if (err || doc == null) {
                 console.warn("database error in command rank");
                 return 15;
             }
-            msg.channel.send(`<@!${mentioned || msg.author.id}> hat **${doc.xp}XP**, noch ${getXpToLevelUp(doc.xp)}`);
+            msg.channel.send(`${mentionedname} hat **${doc.xp}XP**, noch ${getXpToLevelUp(doc.xp)}`);
         });
     }
 };
--- index.js
+++ index.js
@@ -16,39 +16,68 @@ client.on("ready", () => {
     console.log("im ready!!");
 });
 
-async function levelUp(msg) {
-    console.log(msg);
+function rankUp(msg, xp) {
+    const member = msg.guild.members.cache.find(mem => mem.user.id === msg.author.id);
+    let role;
+    switch(Math.floor(xp/1000)) {
+        /*
+            40 DeineMutter
+            30 türkis
+            20 gold
+            10 blau (hihi)
+            5 orange
+        /**/
+        case 40:
+            role = msg.guild.roles.cache.find(role => role.name === 'DeineMutter');
+            member.roles.remove(msg.guild.roles.cache.find(role => role.name === 'türkis'));
+            break;
+        case 30:
+            role = msg.guild.roles.cache.find(role => role.name === 'türkis');
+            member.roles.remove(msg.guild.roles.cache.find(role => role.name === 'gold'));
+            break;
+        case 20:
+            role = msg.guild.roles.cache.find(role => role.name === 'gold');
+            member.roles.remove(msg.guild.roles.cache.find(role => role.name === 'blau (hihi)'));
+            break;
+        case 10:
+            role = msg.guild.roles.cache.find(role => role.name === 'blau (hihi)');
+            member.roles.remove(msg.guild.roles.cache.find(role => role.name === 'orange'));
+            break;
+        case 5:
+            role = msg.guild.roles.cache.find(role => role.name === 'orange');
+    }
+    if (role) member.roles.add(role);
+    msg.channel.send(`HGW! ${msg.author} du bist nun lvl ${Math.floor(xp/1000)}`);
+}
+
+function levelUp(msg) {
     database.findOne({userid: msg["author"]["id"]}, (err, data) => {
-        console.log(data);
         let newXP = Math.floor(Math.random()*10)+15;
         if (err || data == null) {
             database.insert({ userid: msg["author"]["id"], xp: newXP });
         } else {
             let uxp = data["xp"];
-            if (data.time - Date.now() > 1000*60) {
-                database.update(
-                    { userid: msg["author"]["id"] },
-                    { $set:{
-                        time: Date.now(),
-                        xp: uxp + newXP
-                    }},
-                    { upsert: false, multi: false }, function () {}
-                );
-            } else {
-                database.update(
-                    { userid: msg["author"]["id"] },
-                    { $set:{
-                        time: Date.now(),
-                        xp: uxp + 1
-                    }},
-                    { upsert: false, multi: false }, function () {}
-                );
+            if (Date.now() - data.time < 1000*60) {
+                newXP = 1;
             }
+            if ((uxp + newXP) % 1000 < newXP) {
+                rankUp(msg, uxp+newXP);
+            }
+            database.update(
+                { userid: msg["author"]["id"] },
+                { $set:{
+                    time: newXP > 1 ? Date.now() : data.time,
+                    xp: uxp + newXP
+                }},
+                { upsert: false, multi: false }, function () {}
+            );
         }
     });
 }
 
 client.on("message", (msg) => {
-    levelUp(msg);
-    commandHandler(msg, {db: database});
+    if (!msg.author.bot) {
+        levelUp(msg);
+        commandHandler(msg, {db: database});
+    }
 });
